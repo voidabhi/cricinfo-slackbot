8d5996fc3cf6b6219bb161e4f08dbde208b355db
Abhijeet Mohan <void.aby@gmail.com>
2016-08-15 20:15:21 +0530

    Update deploy.sh

--- .bin/deploy.sh
+++ .bin/deploy.sh
@@ -5,6 +5,8 @@ file_exists=test -e "Procfile"
 
 # check if heroku branch exists
 remote_exists=`git show-ref refs/heads/heroku`
+
+# push if file and remote exists
 if [ "$file_exists" & "$remote_exists" ]; then
     # push to heroku
     git push heroku master
